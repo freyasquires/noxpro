854b76fa88bb0dca199c30554a324c20a8a5adfa
Graeme Nott <graeme.nott@faam.ac.uk>
2018-01-06 17:28:52 +0000

    Added comment about NANs

--- R/StateChange.R
+++ R/StateChange.R
@@ -1,18 +1,21 @@
 #' Row numbers of change
 #' 
-#' Returns the row number of a change in value.
+#' Returns the row number of a change in value in a given column.
 #'  
 #' @name StateChange
 #' @param col Column of values
-#' 
 #' 
 #' @return Row numbers where change happens for col.
 #' 
 #' @export 
 
 StateChange = function(col){
-  delta = diff(col)
-  deltarow = which(delta!=0)
+
+  delta <- diff(col)
+  # Obtain rows that are not zero
+  # Note that this includes nan's but they are ignored (?)
+  deltarow <- which(delta!=0)
+  
   return(deltarow)
 }
 
